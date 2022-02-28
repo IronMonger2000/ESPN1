b2646195a24896d2e1007f04414eeaff24b8926e
IronMonger2000 <bhaskar.nv7@gmail.com>
2022-02-28 18:09:32 +0530

    modified first

---
+++ scorecard.js
@@ -0,0 +1,63 @@
+// const url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595/mumbai-indians-vs-chennai-super-kings-1st-match-1216492/full-scorecard";
+const request = require('request')
+const cheerio = require('cheerio');
+const { contains } = require('cheerio');
+function processScorecardUrl(url) {
+    request(url, cb);
+}
+function cb(error, respose, html) {
+    if (error)
+        console.log(error);
+    else
+        extractLink(html);
+}
+
+function extractLink(html) {
+    let selTool = cheerio.load(html);
+    let desc = selTool('.header-info .description').text();
+    let descStrToArr = desc.split(",");
+    let venue = descStrToArr[1].trim();
+    let date = descStrToArr[2].trim();
+    let result = selTool('.match-info.match-info-MATCH.match-info-MATCH-half-width .status-text').text();
+    console.log(venue);
+    console.log(date);
+    console.log(result);
+
+    let htmlString = "";
+    let innings = selTool('.card.content-block.match-scorecard-table>.Collapsible');
+    for (let i = 0; i < innings.length; i++) {
+        // let teamname = selTool(innings[i]).find('.Collapsible .header-title.label').text().split('INNINGS');
+        // console.log(teamname[0]);
+        //or
+        let teamname = selTool(innings[i]).find('h5').text().split('INNINGS')[0].trim();
+        let opponentIndex = i == 0 ? 1 : 0;
+        let opponentName = selTool(innings[opponentIndex]).find('h5').text().split('INNINGS')[0].trim();
+        console.log(teamname, opponentName);
+
+        let CInning = selTool(innings[i])
+        let allRows = CInning.find('table.batsman tbody tr');
+
+        for (let j = 0; j < allRows.length; j++) {
+            let allCols = selTool(allRows[j]).find('td');
+            let isBatsman = selTool(allCols[0]).hasClass('batsman-cell');
+            if (isBatsman == true) {
+                let batsManName = selTool(allCols[0]).text().trim();
+                let runs = selTool(allCols[2]).text().trim();
+                let balls = selTool(allCols[3]).text().trim();
+                let fours = selTool(allCols[5]).text().trim();
+                let sixes = selTool(allCols[6]).text().trim();
+                let StrikeRate = selTool(allCols[7]).text().trim();
+                console.log(` ${batsManName} | ${runs} | ${balls} | ${fours} | ${sixes} | ${StrikeRate}`);
+            }
+        }
+        console.log(`-------------------------------------------------------------------------------------`)
+    }
+    console.log(`((((((((((((((((((((((((((((((((((((()))))))))))))))))))))))))))))))))))))`)
+    // console.log(htmlString);
+}
+
+
+module.exports = {
+    processThisLink: processScorecardUrl
+}
+
